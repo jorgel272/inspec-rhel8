277aceb356e705c3b2ae521844d9b050bb401c75
Jorge Liauw Calo <info@jorgeliauw.nl>
2023-09-30 22:53:46 +0200

    Update Ensure_SSH_MaxSessions_is_set.rb

--- controls/Ensure_SSH_MaxSessions_is_set.rb
+++ controls/Ensure_SSH_MaxSessions_is_set.rb
@@ -3,8 +3,8 @@ control "RHEL8 Check SSH_MaxSessions_is_set_to_4_or_less" do
   desc  "Protect against DDoS by opening a large amount of SSH sessions"
   impact 'high'
   tag scc_category: 'OPEN_SSH_PORT'
-  tag resource_type: 'compute'
-  tag project_id: project_id
+  tag resource_type: 'compute.Network'
+ #tag project_id: project_id
 
   describe sshd_config do
     its('MaxSessions') { should cmp <= 4 }
