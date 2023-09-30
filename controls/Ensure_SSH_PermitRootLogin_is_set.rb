742270fa8e1aa329fca12ca37bcd6a22d13a0e19
Jorge Liauw Calo <info@jorgeliauw.nl>
2023-09-30 23:11:24 +0200

    Create Ensure_SSH_PermitRootLogin_is_set.rb

---
+++ controls/Ensure_SSH_PermitRootLogin_is_set.rb
@@ -0,0 +1,11 @@
+control "RHEL8_Check_SSH_Permit_Root_Login_is_disabled" do
+  title "RHEL8 Ensure SSH Permit Root Login_is_disabled"
+  desc  "Check that SSH root login is disabled"
+  impact 'critical'
+  tag scc_category: 'OPEN_SSH_PORT'
+  tag resource_type: 'compute.Network'
+
+  describe sshd_config do
+    its('PermitRootLogin') { should eq 'no' }
+  end
+end
