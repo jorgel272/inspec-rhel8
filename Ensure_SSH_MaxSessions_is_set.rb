4ff8716d3bfa3faba418d96fc01d752a01cac42c
Jorge Liauw Calo <info@jorgeliauw.nl>
2023-09-30 21:31:37 +0200

    Add files via upload

---
+++ Ensure_SSH_MaxSessions_is_set.rb
@@ -0,0 +1,13 @@
+control "RHEL8 Check SSH_MaxSessions_is_set_to_4_or_less" do
+  title "RHEL8 Check SSH_MaxSessions_is_set_to_4_or_less"
+  desc  "Protect against DDoS by opening a large amount of SSH sessions"
+  impact 'high'
+  tag scc_category: 'OPEN_SSH_PORT'
+  tag resource_type: 'compute'
+  tag project_id: project_id
+
+  describe sshd_config do
+    its('MaxSessions') { should cmp <= 4 }
+    its('MaxSessions') { should cmp > 0 }
+  end
+end
