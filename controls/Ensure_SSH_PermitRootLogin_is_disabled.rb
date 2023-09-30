f55e26735b3df71fcbfedf964d90d2d1a38c4a48
Jorge Liauw Calo <info@jorgeliauw.nl>
2023-09-30 23:17:12 +0200

    Update Ensure_SSH_PermitRootLogin_is_disabled.rb

--- controls/Ensure_SSH_PermitRootLogin_is_disabled.rb
+++ controls/Ensure_SSH_PermitRootLogin_is_disabled.rb
@@ -4,6 +4,7 @@ control "RHEL8_Check_SSH_Permit_Root_Login_is_disabled" do
   impact 'critical'
   tag scc_category: 'OPEN_SSH_PORT'
   tag resource_type: 'compute.Network'
+  tag findingClass: 'MISCONFIGURATION'
 
   describe sshd_config do
     its('PermitRootLogin') { should eq 'no' }
