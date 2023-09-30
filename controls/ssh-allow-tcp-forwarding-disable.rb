d82474eb2cb3c35ad5f9401277ef8fd1c4071444
Jorge Liauw Calo <info@jorgeliauw.nl>
2023-09-30 23:58:50 +0200

    Update ssh-allow-tcp-forwarding-disable.rb

--- controls/ssh-allow-tcp-forwarding-disable.rb
+++ controls/ssh-allow-tcp-forwarding-disable.rb
@@ -1,7 +1,7 @@
 control "RHEL8_Check_SSH_Allow_Tcp_Forwarding_is_disabled" do
   title "RHEL8 Ensure SSH Allow Tcp Forwarding_is_disabled"
   desc  "Check that SSH TCP Forwarding is disabled"
-  impact 'critical'
+  impact 'high'
   tag scc_category: 'OPEN_SSH_PORT'
   tag resource_type: 'compute.Network'
   tag finding_class: 'MISCONFIGURATION'
